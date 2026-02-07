52c396baa067139d1ca33cbce92bb1453c25afc2
maste <masterbuildrp@gmail.com>
2026-02-06 17:47:52 -0800

    block

--- src/main/java/net/codercadet/enchantcraft/block/ModBlocks.java
+++ src/main/java/net/codercadet/enchantcraft/block/ModBlocks.java
@@ -1,10 +1,12 @@
 package net.codercadet.enchantcraft.block;
 
-import com.jcraft.jorbis.Block;
 import net.codercadet.enchantcraft.EnchantCraft;
 import net.codercadet.enchantcraft.item.ModItems;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.DropExperienceBlock;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.neoforged.bus.api.IEventBus;
@@ -16,21 +18,22 @@ import java.util.function.Supplier;
 public class ModBlocks {
     public static final DeferredRegister.Blocks BLOCKS =
             DeferredRegister.createBlocks(EnchantCraft.MOD_ID);
-    public static final DeferredBlock<net.minecraft.world.level.block.Block> ENCHANTED_ORE = registerBlock("enchanted_ore",
-            () -> new net.minecraft.world.level.block.Block(BlockBehaviour.Properties.of()
-                        .strength(4f).requiresCorrectToolForDrops().sound(SoundType.DEEPSLATE)));
+
+    public static final DeferredBlock<Block> ENCHANTED_ORE = registerBlock("enchanted_ore",
+            () -> new Block(BlockBehaviour.Properties.of()
+                    .strength(4f).requiresCorrectToolForDrops().sound(SoundType.STONE)));
+
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block) {
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
-        registerBlockitem(name, toReturn);
+        registerBlockItem(name, toReturn);
         return toReturn;
     }
 
-    private static <T extends Block> void registerBlockitem(String name, DeferredBlock<T> block) {
+    private static <T extends Block> void registerBlockItem(String name, DeferredBlock<T> block) {
         ModItems.ITEMS.register(name, () -> new BlockItem(block.get(), new Item.Properties()));
     }
 
     public static void register(IEventBus eventBus) {
         BLOCKS.register(eventBus);
-
     }
 }
