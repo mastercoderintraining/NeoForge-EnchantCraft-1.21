15e8147837f27bb7ce094f2dbbad3da267fa6bf2
Daniel <danielpaulsen@gmail.com>
2026-02-20 13:16:40 -0800

    recipe update

--- src/main/java/net/codercadet/enchantcraft/EnchantCraft.java
+++ src/main/java/net/codercadet/enchantcraft/EnchantCraft.java
@@ -41,8 +41,8 @@ public class EnchantCraft {
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
-        ModItems.register(modEventBus);
-        ModBlocks.register(modEventBus);
+        //ModItems.register(modEventBus);
+        //ModBlocks.register(modEventBus);  *****************
 
 
         // Register the item to a creative tab
@@ -56,12 +56,12 @@ public class EnchantCraft {
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-        if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+        /*if (event.getTabKey() == CreativeModeTabs.INGREDIENTS) { ************************
             event.accept(ModItems.ENCHANTED_DIAMOND);
         }
         if (event.getTabKey() == CreativeModeTabs.NATURAL_BLOCKS) {
             event.accept(ModBlocks.ENCHANTED_ORE);
-        }
+        }*/
 
     }
 
